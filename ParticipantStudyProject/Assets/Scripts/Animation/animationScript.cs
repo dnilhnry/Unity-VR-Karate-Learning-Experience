17325a3049e8c9f7ac8f04d33d6932e8fb5c47e4
Daniel <dnilhnry@icloud.com>
2023-03-24 17:25:07 +0000

    Progress

--- ParticipantStudyProject/Assets/Scripts/Animation/animationScript.cs
+++ ParticipantStudyProject/Assets/Scripts/Animation/animationScript.cs
@@ -4,15 +4,103 @@ using UnityEngine;
 
 public class animationScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    // documentation
+    //-------------------------------------------------------------
+
+    // https://docs.unity3d.com/ScriptReference/Animator.html
+
+    //-------------------------------------------------------------
+
+
+    // initialise
+    //-------------------------------------------------------------
+
+    Animator animatorController;
+
     void Start()
     {
-        
+        animatorController = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //-------------------------------------------------------------
+
+
+    // technique button functions
+    //-------------------------------------------------------------
+
+    public void SetAGEUKE()
     {
-        
+        animatorController.Play("Base Layer.AGE-UKE");
     }
+
+    public void SetGEDANBARAI()
+    {
+        animatorController.Play("Base Layer.GEDAN-BARAI");
+    }
+
+    public void SetSOTOUKE()
+    {
+        animatorController.Play("Base Layer.SOTO-UKE");
+    }
+
+    public void SetUCHIUKE()
+    {
+        animatorController.Play("Base Layer.UCHI-UKE");
+    }
+
+    //-------------------------------------------------------------
+
+
+    // playback speed button functions
+    //-------------------------------------------------------------
+
+    public void Set025()
+    {
+        animatorController.SetFloat("speedMultiplier", 0.25f);
+    }
+
+    public void Set050()
+    {
+        animatorController.SetFloat("speedMultiplier", 0.50f);
+    }
+
+    public void Set075()
+    {
+        animatorController.SetFloat("speedMultiplier", 0.75f);
+    }
+
+    public void Set100()
+    {
+        animatorController.SetFloat("speedMultiplier", 1.00f);
+    }
+
+    public void Set125()
+    {
+        animatorController.SetFloat("speedMultiplier", 1.25f);
+    }
+
+    public void Set150()
+    {
+        animatorController.SetFloat("speedMultiplier", 1.50f);
+    }
+
+    //-------------------------------------------------------------
+
+
+    // lead hand button functions
+    //-------------------------------------------------------------
+
+    public void SetLeft()
+    {
+        animatorController.SetBool("mirrored", false);
+    }
+
+    public void SetRight()
+    {
+        animatorController.SetBool("mirrored", true);
+    }
+
+    //-------------------------------------------------------------
+
 }
