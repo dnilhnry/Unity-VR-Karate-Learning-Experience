a9da97179ab0d7b041b50a53f8f36269cef2ea00
Daniel <dnilhnry@icloud.com>
2023-04-12 17:59:22 +0100

    Formatting

--- ParticipantStudyProject/Assets/Scripts/Main/mainScript.cs
+++ ParticipantStudyProject/Assets/Scripts/Main/mainScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class mainScript : MonoBehaviour
+public class MainScript : MonoBehaviour
 {
 
     // documentation
